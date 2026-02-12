afa4e7f5f08e144494b4d6c87a434575f825ad72
Fedulov Danil <danya7777kg@gmail.com>
2026-02-12 21:56:51 +0600

    Задание 2 шифр

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const Vigenere = require('caesar-salad').Vigenere;
 const app = express();
 const port = 8000;
+const PASSWORD = 'asd';
+
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
+});
+
+
+app.get('/encode/:message', (req, res) => {
+  const encodeText = req.params.message;
+  const encoded = Vigenere.Cipher(PASSWORD).crypt(encodeText);
+  res.send(encoded);
+});
+
+app.get('/decode/:message', (req, res) => {
+  const decodeText = req.params.message;
+  const decoded = Vigenere.Decipher(PASSWORD).crypt(decodeText);
+  res.send(decoded);
 });
 
 app.get('/:text', (req, res) => {
